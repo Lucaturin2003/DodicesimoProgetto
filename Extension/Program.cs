cf73efa21a69bce98a20f2c1e9d1a83f58f54420
Luca Turin <luca@payoff.it>
2024-02-07 11:14:25 +0100

    Extension

---
+++ Extension/Program.cs
@@ -0,0 +1,24 @@
+﻿using Extension;
+
+string post = "This is supposed to be a very long post bla bla bla ...";
+var shortenedPost = post.Shorten(5);
+Console.WriteLine(shortenedPost);
+
+namespace Extension
+{
+    public static class StringExtensions
+    {
+        public static string Shorten(this string str, int numberOfWords) 
+        {
+            if(numberOfWords < 0) throw new ArgumentOutOfRangeException("Number of words should be greater or equal than 0");
+
+            if(numberOfWords == 0) return str;
+
+            var words = str.Split(' ');
+
+            if(words.Length <= numberOfWords) return str;
+
+            return string.Join(" ", words.Take(numberOfWords));
+        }
+    }
+}
